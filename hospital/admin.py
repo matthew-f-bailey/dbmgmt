a18a430234514c1c523ae67907ed278ba9dcaf63
Motaz Abulibeh <mabulibeh@gmail.com>
2023-04-10 14:35:39 -0500

    add fields to places models, templates, and view for home page

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models.places import *
+
+# adding Clinic, Unit, Room tables to admin page
+adminpage = {Clinic, Unit, Room }
+admin.site.register(adminpage)
--- hospital/models/places.py
+++ hospital/models/places.py
@@ -2,12 +2,31 @@ from django.db import models
 
 
 class Clinic(models.Model):
-    pass
+    email = models.EmailField(max_length=254)
+    name = models.CharField(max_length=200)
+    phone = models.CharField(max_length=30)
+    address = models.CharField(max_length=254)
 
+# unit in clinc
+class Unit(models.Model):
+    name = models.CharField(max_length=254)
+    clinic = models.ForeignKey(
+        Clinic, 
+        on_delete =  models.CASCADE
+    )
 
+# room inside unit
 class Room(models.Model):
-    pass
+    number = models.IntegerField()
+    unit = models.ForeignKey(
+        Unit,
+        on_delete=models.CASCADE
+    )
 
-
+# bed in a room
 class Bed(models.Model):
-    pass
+    bedLetter = models.CharField(max_length=30)
+    room = models.ForeignKey(
+        Room, on_delete=models.CASCADE
+    )
+    
