614b76d06578717a952f83553b8d183f2c75edf3
matthew-f-bailey <matthew.f.bailey@outlook.com>
2023-04-14 23:59:48 -0400

    Started generic post/get form-view combo. WIP - May revert

--- hospital/views/people_view.py
+++ hospital/views/people_view.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.urls import reverse
 from hospital.forms.people_form import PatientForm
 
 
 def create_patient_view(request):
-    patient_form = PatientForm()
-    return render(request, "create_patient.html", {"patient_form": patient_form})
+
+    # Navigations to page
+    if request.method == "GET":
+        patient_form = PatientForm()
+        return render(
+            request,
+            "create_form.html",
+            {
+                "model_form": patient_form,
+                "post_to": reverse("create_patient_view")
+            }
+        )
+
+    patient_form = PatientForm(request)
+
