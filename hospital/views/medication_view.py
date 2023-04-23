ec9adb0a1e895ab92134b5ca904b5bcfcce2608f
Motaz Abulibeh <mabulibeh@gmail.com>
2023-04-22 21:11:43 -0500

    updated medications views to use class-based views

--- hospital/views/medication_view.py
+++ hospital/views/medication_view.py
@@ -4,32 +4,20 @@ from hospital.forms.medication_form import MedicationForm, InteractionForm
 from django.shortcuts import redirect, render
 from django.contrib import messages
 from django.views.generic import ListView, DetailView
-from hospital.models.illnesses import Medication
+from django.views.generic.edit import CreateView
+from hospital.models.illnesses import Medication, Interactions
+from django.http import HttpResponseRedirect
 
 # To add a new medication
-def create_medication_view(request):
-    if request.method == "GET":
-        medication_form = MedicationForm()
-        return render(
-            request,
-            "create_form.html",
-            {
-                "header" : "Create Medication",
-                "model_form": medication_form,
-                "post_to": reverse("create_medication_view")
-            }
-        )
+class MedicationCreateView(CreateView):
+    model = Medication
+    form_class = MedicationForm
 
-    elif request.method == "POST":
-        medication_form = MedicationForm(request.POST)
-        if medication_form.is_valid():
-            medication_form.save()
-            print(medication_form)
-            # Once a medication is created redirect to medicatoin interacion page
-            return redirect("create_medication_interaction")
-        
+    def get_success_url(self):
+        return reverse('view_medications', kwargs={'pk': self.object.id})
+
+
 # class based view to view medications
-
 class MedicationListView(ListView):
     model = Medication
     template_name = 'view_list.html'
@@ -40,31 +28,42 @@ class MedicationDetailView(DetailView):
 
 
 # To add interaction between medications
-def create_medication_interaction(request):
-    if request.method == "GET":
-        interaction_form = InteractionForm()
-        return render(
-            request,
-            "create_form.html",
-            {
-                "header" : "Add Medication Interactions",
-                "model_form": interaction_form,
-                "post_to": reverse("create_medication_interaction")
-            }
-        )
-    elif request.method == "POST":
-        interaction_form = InteractionForm(request.POST)
-        if interaction_form.is_valid():
-            print (interaction_form.data)
-            # Check if the same mediciation is selected
-            if (interaction_form.data.get('medication1') == interaction_form.data.get('medication2')):
-                messages.error(request, "Can't assign interaction between the same medicine")
-            else:
-                interaction_form.save()
-            return redirect("create_medication_interaction")
-        # If interaction already exist return a message and redirct to the same page
-        else:
-            messages.error(request, 'This interaction already exists')
-            return redirect("create_medication_interaction")
+
+class CreateMedicationInteraction(CreateView):
+    model = Interactions
+    form_class = InteractionForm
+
+    #
+    
+    def get_success_url(self):
+        return reverse('create_medication_interaction')
+
+
+# def create_medication_interaction(request):
+#     if request.method == "GET":
+#         interaction_form = InteractionForm()
+#         return render(
+#             request,
+#             "create_form.html",
+#             {
+#                 "header" : "Add Medication Interactions",
+#                 "model_form": interaction_form,
+#                 "post_to": reverse("create_medication_interaction")
+#             }
+#         )
+#     elif request.method == "POST":
+#         interaction_form = InteractionForm(request.POST)
+#         if interaction_form.is_valid():
+#             print (interaction_form.data)
+#             # Check if the same mediciation is selected
+#             if (interaction_form.data.get('medication1') == interaction_form.data.get('medication2')):
+#                 messages.error(request, "Can't assign interaction between the same medicine")
+#             else:
+#                 interaction_form.save()
+#             return redirect("create_medication_interaction")
+#         # If interaction already exist return a message and redirct to the same page
+#         else:
+#             messages.error(request, 'This interaction already exists')
+#             return redirect("create_medication_interaction")
 
 
